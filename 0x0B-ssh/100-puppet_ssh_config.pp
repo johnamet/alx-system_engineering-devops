16edd07d455e9d534d64792b855849450d385597
Johnny <johnametepeagboku@live.com>
2024-01-23 16:34:31 +0000

    fixup: Task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 #the scripts sets up my client ssh configuration file so that i can
 # connect to a server without typing a password
 
+file_line {'Log passless auth':
+  path => '~/.ssh/school',
+  line => 'PasswordAuthentication no'
+}
+
 file_line {'Config Identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school'
 }
 
-file_line {'Log passless auth':
-  path => '~/.ssh/school',
-  line => 'PasswordAuthentication no'
-}
+
