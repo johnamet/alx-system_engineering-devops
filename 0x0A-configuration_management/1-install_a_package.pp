7d7256395986ecf1a1a3ac4ba39b1ad35110a1c9
johnny <johnametepeagboku@gmail.com>
2024-01-25 11:57:06 +0000

    Fixup Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # installs a flask package
 
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+# Install Flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
