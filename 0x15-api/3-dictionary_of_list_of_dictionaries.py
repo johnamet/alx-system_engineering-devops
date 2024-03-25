8813ad90bdcb808894cf1ce706f84bc0051aa625
johnny <johnametepeagboku@gmail.com>
2024-03-25 21:51:29 +0000

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,12 +8,9 @@ import requests
 import sys
 
 
-def gather_data_to_json(user_id):
+def gather_data_to_csv():
     """
     Gather data from an API about a user's todo list.
-
-    Args:
-        user_id (int): The ID of the user.
 
     Returns:
         None
@@ -22,23 +19,24 @@ def gather_data_to_json(user_id):
     base_url = "https://jsonplaceholder.typicode.com"
 
     # URLs for user and todo requests
-    user_req_url = f"{base_url}/users/{user_id}"
+    user_req_url = f"{base_url}/users"
     todo_req_url = f"{base_url}/todos"
 
     # Fetch user data
-    user_response = requests.get(user_req_url)
-    user_json = json.loads(user_response.text)
+    users_response = requests.get(user_req_url)
+    users_json = json.loads(users_response.text)
 
     # Fetch todo list data
     todos_response = requests.get(todo_req_url)
     todos_json = json.loads(todos_response.text)
 
-    with open(f"{user_id}.json", mode="w", newline="") as file:
-        data = {f'{user_id}': [{"task": todo["title"],
-                                "completed": todo["completed"],
-                                "username": user_json["username"]}
-                               for todo in todos_json
-                               if todo["userId"] == user_id]}
+    with open("todo_all_employees.json", mode="w", newline="") as file:
+        data = {f'{user["id"]}': [{"username": user["username"],
+                                   "task": todo["title"],
+                                   "completed": todo["completed"]}
+                                  for todo in todos_json
+                                  if todo["userId"] == user["id"]]
+                for user in users_json}
         json.dump(data, file)
         # csv.writer.writerow(header)
         # for todo in todos_json:
@@ -51,10 +49,9 @@ def gather_data_to_json(user_id):
 
 if __name__ == "__main__":
     # Get user ID from command line argument
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
-    user_id = int(sys.argv[1])
+    # if len(sys.argv) != 2:
+    #     print("Usage: python script.py <user_id>")
+    #     sys.exit(1)
 
     # Gather and display user's todo list
-    gather_data_to_json(user_id)
+    gather_data_to_csv()
