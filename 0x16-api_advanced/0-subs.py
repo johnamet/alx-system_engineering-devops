7318700faa860af2caf9b5f817769f8cfe745fd9
Johnny <johnametepeagboku@live.com>
2024-04-11 03:08:36 +0000

    Task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,23 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    The function gets the total number
-    of subscribers from a subreddit
-    Args:
-        subreddit (str): The subreddit to query
-    Returns:
-        The total number of subscribers to the subreddit
-    """
-    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    request = requests.get(base_url,
-                           headers={'User-agent': 'johnny@johnny'},
-                           allow_redirects=False)
-
-    if request.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    sub_json = request.json()
-    data = sub_json["data"]
-
-    return data["subscribers"]
+    results = response.json().get("data")
+    return results.get("subscribers")
