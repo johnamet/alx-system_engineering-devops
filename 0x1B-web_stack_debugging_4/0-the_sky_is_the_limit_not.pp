af62cef51aa792d69dae9d7851db76a8496cb2c1
Johnny <johnametepeagboku@live.com>
2024-04-18 17:40:11 +0000

    Task: Fixup

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 # the worker processes in the server conf file
 
 exec { 'fix--for-nginx':
-   command => "sed -i 's/worker_processes 4/worker_processes 7/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-   unless  => "grep 'worker_processes 7' /etc/nginx/nginx.conf",
-   path    => ['/bin', '/usr/bin']
+command => "sed -i 's/worker_processes 4/worker_processes 7/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+unless  => "grep 'worker_processes 7' /etc/nginx/nginx.conf",
+path    => ['/bin', '/usr/bin']
 }
