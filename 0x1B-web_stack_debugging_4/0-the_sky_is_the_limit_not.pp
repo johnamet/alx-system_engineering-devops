852e4c37f082b784c4a2d9d6b0ad82a81e27d5f7
Johnny <johnametepeagboku@live.com>
2024-04-18 17:44:18 +0000

    Task: Fixup

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,5 @@
-# The puppet file fixes server requests by increasing
-# the worker processes in the server conf file
-
+# Fixing the number of failed requests to get to 0
 exec { 'fix--for-nginx':
-command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-unless  => "grep 'worker_processes 7;' /etc/nginx/nginx.conf",
-path    => ['/bin', '/usr/bin']
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
